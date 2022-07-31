fe4eee48be3f984bbba098b0bf9e1703502cd083
BRK1077 <98328273+BRK1077@users.noreply.github.com>
2022-07-31 14:48:57 +0530

    updated VPC and subnet

--- main.tf
+++ main.tf
@@ -34,18 +34,30 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
-resource "aws_default_subnet" "default_az1" {
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Default subnet for east-us-1"
+    Name = "tf-example"
   }
 }
+
 # Create AWS ec2 instance
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
+  subnet_id = aws_subnet.my_subnet.id
   security_groups= [var.security_group]
   tags= {
     Name = var.tag_name
